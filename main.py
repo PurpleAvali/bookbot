fb60f4989db7b4fd2b18314aceba8ae66e5cd19b
purpleavali <purpledragon20+git@proton.me>
2025-12-19 07:39:51 +0100

    Final commit book bot assignment

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from stats import count_words_in_string
 from stats import count_chars_in_string
 from stats import ordered_list_of_dicts
 from stats import print_pretty_dict
+import sys
 
 def get_book_text(filepath: str) -> str:
     
@@ -20,17 +21,23 @@ def get_book_text(filepath: str) -> str:
 
 
 def main():
-    filepath = "books/frankenstein.txt"
-    book_contents = get_book_text(filepath=filepath)
-    num_words = count_words_in_string(book_contents)
-    num_chars = count_chars_in_string(book_contents)
-    char_list = ordered_list_of_dicts(num_chars)
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {filepath}...")
-    print("----------- Word Count ----------")
-    print(f"Found {num_words} total words")
-    print("--------- Character Count -------")
-    print_pretty_dict(char_list)
+    if len(sys.argv) > 1:
+        filepath = sys.argv[1]
+        book_contents = get_book_text(filepath=filepath)
+        num_words = count_words_in_string(book_contents)
+        num_chars = count_chars_in_string(book_contents)
+        char_list = ordered_list_of_dicts(num_chars)
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {filepath}...")
+        print("----------- Word Count ----------")
+        print(f"Found {num_words} total words")
+        print("--------- Character Count -------")
+        print_pretty_dict(char_list)
+        sys.exit(0)
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
 
 if __name__ == "__main__":
     main()
